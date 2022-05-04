100cfc660eb6d6b9f19cb5590ef08702a2009bf1
Carlos Santander <carlos.santander@gmail.com>
2022-05-04 11:40:44 -0500

    update logs

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -1,9 +1,33 @@
 import { Handler } from 'express';
+import addrs, { ParsedMailbox } from 'email-addresses';
 
 export const parse: Handler = (req, res) => {
 	const body = req.body;
 
-	console.log(body);
+	const from: string = body.from;
+	const senderParts = addrs.parseOneAddress(from);
+	const emailSender = (senderParts as ParsedMailbox).address;
+	console.log('emailSender =>', emailSender);
+
+	const to: string = body.to;
+	const associationSlug = to.split('@')[0];
+	console.log('associationSlug =>', associationSlug);
+
+	const subject: string = body.subject;
+	console.log('subject =>', subject);
+
+	const text: string = body.text;
+	console.log('description =>', text);
+
+	const ccs: string[] = body.cc.split(',');
+	const emailsCCs = ccs.map(cc => {
+		const ccParts = addrs.parseOneAddress(cc);
+		return (ccParts as ParsedMailbox).address;
+	});
+	console.log('emailsCCs =>', emailsCCs);
+
+	const attachments = parseInt(body.attachments, 10);
+	console.log('attachments =>', attachments);
 
 	if (req.files && req.files.length > 0) {
 		console.log(req.files);
